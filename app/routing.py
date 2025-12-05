01590962f455cfedd161cabfa913484e9421676a
paolo-fx <paolo.faux@gmail.com>
2025-12-05 18:15:10 +0100

    Add route optimization module with placeholder function
    Initial implementation of route optimization module.

---
+++ app/routing.py
@@ -0,0 +1,12 @@
+"""
+Module d'optimisation d'itinéraires.
+Version simplifiée pour la version open source.
+"""
+
+def optimize_route(coords):
+    """
+    Prend une liste de coordonnées (lat, lon)
+    et renvoie un ordre de visite optimisé (mock).
+    """
+    # TODO : implémenter un vrai algo (TSP, etc.)
+    return coords
